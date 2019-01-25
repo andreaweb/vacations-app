f080758c3ef87fa3d7fa9fefec1020b3495c8b83
andreaweb <deiainluv@gmail.com>
2019-01-25 08:41:32 -0200

    basic components main page

--- src/components/Vacations/Vacations.jsx
+++ src/components/Vacations/Vacations.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
+import Calendar from 'rc-calendar';
+import 'rc-calendar/assets/index.css';
 import './Vacations.scss';
 
 class Vacations extends Component {
@@ -14,7 +16,16 @@ class Vacations extends Component {
   render() {
     return (
       <main className="home">
-       
+        <h1>Vacations</h1>
+        <Calendar />
+
+        <fieldset>
+          <input type="checkbox" name="show-friends" id="show-friends"/>
+          <label htmlFor="show-friends">Show Friend&apos;s Vacations</label>
+
+          <input type="checkbox" name="show-self" id="show-self"/>
+          <label htmlFor="show-self">Show My Vacations</label>
+        </fieldset>
       </main>
     );
   }
